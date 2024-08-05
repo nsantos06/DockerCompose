995555a33c027a4b5f2c15c97437d2224116e833
Nicollas <nicollasmk9@gmail.com>
2024-08-05 16:21:02 -0300

    Instalado dockerize no Dockerfile e mudado a porta para 3555

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3555
+const config = {
+    host:"db",
+    user:"root",
+    password:"some_password",
+    database:"nodedb"
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Nicollas')`
+connection.query(sql)
+connection.end()
 
 app.get('/', (req,res) =>{
     res.send('<h1>Full Cycle</h1>')  
